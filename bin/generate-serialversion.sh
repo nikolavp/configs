48669aa50f9bcfa8b75d00a1123bca80a8c190e0
Nikola Petrov <nikolavp@gmail.com>
2010-09-17 11:16:04 +0300

    Ups

--- bin/generate-serialversion.sh
+++ bin/generate-serialversion.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # vim: set sw=4 sts=4 et foldmethod=indent :
 
-echo private static final long serialVersionUID $(date +%Y%m%d)L';'
+echo private static final long serialVersionUID = $(date +%Y%m%d)L';'
