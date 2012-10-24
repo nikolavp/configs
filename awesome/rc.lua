94c15e9bfd29afce761a8f23172eb592c68ffbe7
Nikola Petrov <nikolavp@gmail.com>
2012-10-24 11:49:10 +0300

    Small cleanups, solarized color scheme
    * Set a proper browser on 'm' in attachment viewing through mailcap.
    * Changed the colorscheme to solarized.
    * Error detection code to awesome

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -12,6 +12,29 @@ require("revelation")
 vicious = require("vicious")
 
 
+-- Code for debugging errors
+if awesome.startup_errors then
+    naughty.notify({
+        preset = naughty.config.presets.critical,
+        title = 'Oops, there were errors during startup!',
+        text = awesome.startup_errors
+    })
+end
+
+do
+    local in_error = false
+    awesome.add_signal('debug::error', function(err)
+        if in_error then return end
+        in_error = true
+        naughty.notify({
+            preset = naughty.config.presets.critical,
+            title = 'Oops, an error happened!',
+            text = err
+        })
+        in_error = false
+    end)
+end
+
 
 local home = os.getenv("HOME")
 
