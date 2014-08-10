8e2af7801aeee2a4863d4801a71e8b9433cc1574
Nikola Petrov <nikolavp@gmail.com>
2014-08-10 21:33:20 +0300

    Cleanup to bin directory. * Removed old scripts that I no longer use * Added comments to all scripts with description and or usage information

--- bin/eclipse.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# vim: set sw=4 sts=4 et foldmethod=indent :
-
-
-export MOZILLA_DIST=/home/nikolavp/workspace/joci/joci-lib/lib/mozillaparser
-#Stupid settigs come from this and firefox is complaining so set it as a local
-#script
-LD_LIBRARY_PATH="$MOZILLA_DIST:$MOZILLA_DIST/mozilla" /home/nikolavp/Desktop/new-eclipse/eclipse/eclipse
-
