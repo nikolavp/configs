8ea7c1a6ecf885df5b40969f6c01c00699365793
Nikola Petrov <nikolavp@gmail.com>
2014-09-24 09:24:52 +0300

    Added compton(https://github.com/ch) to my configuration

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ done
 mkdir -p $HOME/.config/fontconfig
 test -h $HOME/.config/fontconfig/fonts.conf || ln -sf $PWD/_fonts.conf $HOME/.config/fontconfig/fonts.conf
 test -h $HOME/.config/awesome || ln -sf $PWD/awesome $HOME/.config/awesome
+test -h $HOME/.config/compton.conf || ln -sf $PWD/_compton.conf $HOME/.config/compton.conf
 test -h $HOME/.vim || ln -sf $PWD/vim $HOME/.vim
 test -h $HOME/.vimrc || ln -sf $PWD/vim/.vimrc $HOME/.vimrc
 test -h $HOME/bin || ln -sf $PWD/bin $HOME/bin
