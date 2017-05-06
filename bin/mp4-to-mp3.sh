0904c3db41cfe47cc545c515c4a02dd6cb0a10f1
Nikola Petrov <nikolavp@gmail.com>
2017-05-06 19:15:40 +0300

    Script to convert mp4 to mp3

---
+++ bin/mp4-to-mp3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# vim: set sw=4 sts=4 et foldmethod=indent :
+
+for f in *.mp4
+do
+    name=`echo "$f" | sed -e "s/.mp4$//g"`
+    ffmpeg -i "$f" -vn -ar 44100 -ac 2 -ab 192k -f mp3 "$name.mp3"
+done
