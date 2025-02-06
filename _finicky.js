c767c748fe05118baefd2669d9b3779fa96016d1
Nikola Petrov <nikola@poolside.ai>
2025-02-06 16:26:22 +0200

    Remove this rule

--- _finicky.js
+++ _finicky.js
@@ -7,47 +7,17 @@ module.exports = {
     {
       match: [
         finicky.matchHostnames(["linkedin.com"]),
-        "*.linkedin.com/*"
-      ],
-      browser: "Safari"
-    },
-    {
-      match: [
+        "*.linkedin.com/*",
+
         finicky.matchHostnames(["youtube.com"]),
-        "*.youtube.com/*"
-      ],
-      browser: "Safari"
-    },
-    {
-      match: [
+        "*.youtube.com/*",
+        finicky.matchHostnames(["youtu.be"]),
+        "*.youtu.be/*",
+
         finicky.matchHostnames(["rescuetime.com"]),
         "*.rescuetime.com/*"
       ],
       browser: "Safari"
-    }
-  ],
-
-  rewrite: [
-    {
-      match: ({ url }) => url.protocol === "s3",
-      url({ urlString, url }) {
-        //if (urlString.search(/post-training-datasets.*parquet/) != -1) {
-          //return {
-               //protocol: "https",
-               //host: "podium.poolsi.de",
-               //pathname: `/dataset_viewer`,
-               //search: `dataset_path=${urlString}`
-          //};
-        //}
-        const bucket = url.host;
-        let path = url.pathname.substring(1); // Remove the leading slash
-        return {
-             protocol: "https",
-             host: "us-east-2.console.aws.amazon.com",
-             pathname: `/s3/buckets/${bucket}`,
-             search: `prefix=${path}`
-        };
-      }
     }
   ]
 }
