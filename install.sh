14217d2fb2b102d96f975c8f58c02b84ca59069f
Nikola Petrov <nikola@poolside.ai>
2025-03-02 21:08:58 +0200

    Try out lazy vim

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 # vim: set sw=4 sts=4 et foldmethod=indent :
 
 #General installation of hidden files
-for x in _*;do
-    if [ "$x" = "_fonts.conf" ];then
+for x in _*; do
+    if [ "$x" = "_fonts.conf" ]; then
         continue
     fi
     actual_file=${x/_/.}
-    if [[ -h $HOME/$actual_file ]];then
+    if [[ -L $HOME/$actual_file ]]; then
         echo "Another version for $actual_file was found, skipping"
     else
         ln -sf $PWD/"${x}" $HOME/"${actual_file}"
@@ -18,12 +18,13 @@ mkdir -p $HOME/.config/fontconfig
 test -h $HOME/.config/fontconfig/fonts.conf || ln -sf $PWD/_fonts.conf $HOME/.config/fontconfig/fonts.conf
 test -h $HOME/.config/awesome || ln -sf $PWD/awesome $HOME/.config/awesome
 test -h $HOME/.config/compton.conf || ln -sf $PWD/_compton.conf $HOME/.config/compton.conf
-test -h $HOME/.vim || ln -sf $PWD/vim $HOME/.vim
-test -h $HOME/.vimrc || ln -sf $PWD/vim/.vimrc $HOME/.vimrc
-mkdir -p $HOME/.config
-test -h $HOME/.vim || ln -sf $PWD/vim $HOME/.config/nvim
-mkdir -p ~/.config/nvim
-test -h $HOME/.config/nvim/init.vim || ln -sf $HOME/.vimrc $HOME/.config/nvim/init.vim
+# test -h $HOME/.vim || ln -sf $PWD/vim $HOME/.vim
+# test -h $HOME/.vimrc || ln -sf $PWD/vim/.vimrc $HOME/.vimrc
+# mkdir -p $HOME/.config
+# test -h $HOME/.vim || ln -sf $PWD/vim $HOME/.config/nvim
+# mkdir -p ~/.config/nvim
+# test -h $HOME/.config/nvim/init.vim || ln -sf $HOME/.vimrc $HOME/.config/nvim/init.vim
+test -h $HOME/.config/nvim || ln -sf $PWD/nvim $HOME/.config/nvim
 
 git submodule sync
 git submodule init
@@ -32,4 +33,3 @@ git submodule update
 
 git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
