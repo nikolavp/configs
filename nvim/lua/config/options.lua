e3cd6f97260db3eaec138c3d6b21db46e78da4fd
Nikola Petrov <nikola@poolside.ai>
2025-03-11 13:53:44 +0100

    Added copy pasting with OSC52 instead of a plugin

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,20 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- For some reason this is needed to be able to copy paste on a remote machine in the terminal
+-- see also https://github.com/LazyVim/LazyVim/discussions/2715. Try in a couple of versions if this works
+-- out of the box with lazyvim.
+vim.opt.clipboard = "unnamedplus"
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
