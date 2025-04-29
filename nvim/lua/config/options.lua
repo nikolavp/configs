bb39c9899edc69ab23af0130d833e6349b25febe
Nikola Petrov <nikola@poolside.ai>
2025-04-29 15:31:22 +0300

    These dont work well, remove em

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,15 +6,3 @@
 -- see also https://github.com/LazyVim/LazyVim/discussions/2715. Try in a couple of versions if this works
 -- out of the box with lazyvim.
 vim.opt.clipboard = "unnamedplus"
-
-vim.g.clipboard = {
-  name = "OSC 52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-  },
-}
